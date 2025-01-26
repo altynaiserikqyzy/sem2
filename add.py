a509897586cf0913f356c1e637398b24f5ee9f07
altynaiserikqyzy <altynaiserikqyzyy@gmail.com>
2025-01-26 23:52:43 +0500

    Delete add.py

--- add.py
+++
@@ -1,7 +0,0 @@
-thislist = [ "int" , "str" , "float" , "items"]
-print(thislist)
-thislist.insert(0,"kind of data types:")
-thislist.append("oranges ;)")
-tropical = ["mango", "pineapple", "papaya"]
-thislist.extend(tropical)
-print(thislist)
