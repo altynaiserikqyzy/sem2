6fd9f44adbf092f9c330b293b4527fa09f4c7c53
altynaiserikqyzy <altynaiserikqyzyy@gmail.com>
2025-01-26 23:30:29 +0500

    dictionary

---
+++ labor/loop.py
@@ -0,0 +1,50 @@
+student_info = {
+  "name": "Alice",
+  "age": 22,
+  "city": "London"
+}
+
+# Loop through the dictionary to print all keys
+for key in student_info:
+    print(key)
+
+student_info = {
+  "name": "Alice",
+  "age": 22,
+  "city": "London"
+}
+
+# Loop through the dictionary to print all values
+for key in student_info:
+    print(student_info[key])
+
+student_info = {
+  "name": "Alice",
+  "age": 22,
+  "city": "London"
+}
+
+# Loop through the values using the values() method
+for value in student_info.values():
+    print(value)
+
+student_info = {
+  "name": "Alice",
+  "age": 22,
+  "city": "London"
+}
+
+# Loop through the keys using the keys() method
+for key in student_info.keys():
+    print(key)
+
+student_info = {
+  "name": "Alice",
+  "age": 22,
+  "city": "London"
+}
+
+# Loop through both keys and values using items() method
+for key, value in student_info.items():
+    print(key, value)
+
