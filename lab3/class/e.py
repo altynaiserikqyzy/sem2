a979e87046c09c4d49cb61b9b82907516e898946
altynaiserikqyzy <altynaiserikqyzyy@gmail.com>
2025-02-04 14:05:54 +0500

    Update e.py

---
+++ lab3/class/e.py
@@ -0,0 +1,19 @@
+class Account:
+    def __init__(self, owner, balance=0):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Deposited {amount}. New balance is {self.balance}.")
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Insufficient funds.")
+        else:
+            self.balance -= amount
+            print(f"Withdrew {amount}. New balance is {self.balance}.")
+account = Account("Alice", 100)
+account.deposit(50)
+account.withdraw(30)
+account.withdraw(150)
